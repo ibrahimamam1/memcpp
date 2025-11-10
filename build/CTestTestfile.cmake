6c12acf7fd0c60ac5cd7f919da0dec94023b4fe4
ibrahimamam1 <ibrahima59@iut-dhaka.edu>
2025-11-10 06:18:02 +0600

    gitignore: build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rgb/Desktop/projects/memcpp
-# Build directory: /home/rgb/Desktop/projects/memcpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
